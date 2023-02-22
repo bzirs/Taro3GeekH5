6a4a6712c8f74fc190701d6e3b893e9d9933d01c
bzirs <bzirs@outlook.com>
2023-02-22 20:35:07 +0800

    chore: 🔧 Initialize

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import { memo } from 'react'
+import './app.scss'
+
+const App = (props) => {
+  return props.children
+}
+
+export default memo(App)
---
+++ src/pages/index/index.tsx
@@ -0,0 +1,9 @@
+import { View } from '@tarojs/components'
+import { memo } from 'react'
+import './index.scss'
+
+const Index = () => {
+  return <View>index</View>
+}
+
+export default memo(Index)
